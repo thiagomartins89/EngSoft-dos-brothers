d5c0e9bb74c73833e369c5d8f973a9d153f15dbf
Guilherme Grünewald <g.grunewald@gmail.com>
2013-10-08 22:40:21 -0300

    Person.
    Signed-off-by: Guilherme Grünewald <g.grunewald@gmail.com>

---
+++ OldButGold/src/person/Client.java
@@ -0,0 +1,20 @@
+package person;
+
+public class Client extends Person 
+{
+	
+	private
+		
+	long cnh;	//carteira nacional de habilitação
+	
+	
+	public long getCnh() 
+	{
+		return cnh;
+	}
+
+	public void setCnh(long cnh) 
+	{
+		this.cnh = cnh;
+	}
+}
